cc0e83946d5913477225acd105f174c4e13a73c4
Mike <mike@ring0.cc>
2018-10-24 16:41:27 +1300

    Day 14

---
+++ day_14.py
@@ -0,0 +1,28 @@
+import re, math
+from collections import Counter
+
+MY_TIME = 2503
+finder = re.compile(r'(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.')
+
+def findDist(r, time):
+	name, speed, motion_time, rest_time = r[0], int(r[1]), int(r[2]), int(r[3])
+
+	full_iterations = math.floor(time / (motion_time + rest_time))
+	remain_motion   = min(motion_time, time % (motion_time + rest_time))
+	dist            = speed * (full_iterations * motion_time + remain_motion)
+
+	return (name, dist)
+
+
+
+with open('data/day14.txt') as fp:
+	reindeers = finder.findall(fp.read())
+
+	list1 = [findDist(r, MY_TIME) for r in reindeers]
+	print('Ans 1:', sorted(list1, key=lambda t: t[1], reverse = True)[0])
+
+	list2 = []
+	for time in range(1, MY_TIME):
+		list2.append(sorted((findDist(r, time) for r in reindeers), key=lambda t: t[1], reverse = True)[0][0])
+
+	print('Ans 2:', Counter(list2).most_common(1)[0])
