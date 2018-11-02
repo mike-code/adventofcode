03240ccae561d31ad4510bb223396dae83891bec
Mike <mike@ring0.cc>
2018-11-03 01:10:54 +1300

    Day 19 p2 - I don't know why it works..

--- day_19.py
+++ day_19.py
@@ -2,6 +2,7 @@ import re, random
 from itertools import permutations
 
 data_1 = {}
+data_2 = {}
 
 with open('data/day19.txt') as fp:
 	for i in [i.split(' => ') for i in fp.read().splitlines()]:
@@ -14,3 +15,13 @@ with open('data/day19.txt') as fp:
 res = [re.sub("^(.*?)((%s.*?){%d})%s" % (key, i, key), "\\1\\2%s" % rep, in_str) for key, val in data_1.items() for rep in val for i in range(len(re.findall(key, in_str)))]
 print("Ans 1:", len(set(res)))
 
+def me(thestr, count = 0):
+	if thestr == "e":
+		return count
+
+	for key, val in data_2.items():
+		for i in range(len(re.findall(key, thestr))):
+			return me(re.sub("^(.*?)((%s.*?){%d})%s" % (key, i, key), "\\1\\2%s" % val, thestr), count + 1)
+
+
+print("Ans 2:", me(in_str))
