3a14dd1e10d0e62b0d325d045c3d0c161c60e408
Mike <mike@ring0.cc>
2018-11-02 14:13:02 +1300

    c

--- day_19.py
+++ day_19.py
@@ -14,14 +14,3 @@ with open('data/day19.txt') as fp:
 
 res = [re.sub("^(.*?)((%s.*?){%d})%s" % (key, i, key), "\\1\\2%s" % rep, in_str) for key, val in data_1.items() for rep in val for i in range(len(re.findall(key, in_str)))]
 print("Ans 1:", len(set(res)))
-
-cnt = 0
-
-while in_str != "e":
-	for key, val in data_2.items():
-		for i in range(len(re.findall(key, in_str))):
-			cnt += 1
-			in_str = re.sub("^(.*?)((%s.*?){%d})%s" % (key, i, key), "\\1\\2%s" % val, in_str)
-
-
-# print(cnt)
