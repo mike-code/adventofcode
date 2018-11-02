71b0ff9f0f340a4e579eaac793d9958c1056be64
Mike <mike@ring0.cc>
2018-11-03 03:51:40 +1300

    Day 20 p1 improved

--- day_20.py
+++ day_20.py
@@ -2,6 +2,7 @@ from collections import Counter
 from operator import mul
 from functools import reduce
 
+NUM = 29000000
 
 def prim_geb(n):
 	i = 2
@@ -14,10 +15,9 @@ def prim_geb(n):
 	if n > 1:
 		yield int(n)
 
-i = 0
-res = 0
-while res < 2900000:
-	i += 1
-	res = reduce(mul, [sum([num ** n for n in range(times+1)]) for num,times in Counter([x for x in prim_geb(i)]).items()], 1)
 
-print(i)
+for i in range(NUM):
+	if NUM < 10 * reduce(mul, [sum([num ** n for n in range(times+1)]) for num,times in Counter([x for x in prim_geb(i)]).items()], 1):
+		break
+
+print("Ans 1:", i)
