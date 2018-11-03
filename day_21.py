dbc62be92498a7f9399fa14b31b99f06b51f9a59
Mike <mike@ring0.cc>
2018-11-03 20:59:41 +1300

    Changes Day 21

--- day_21.py
+++ day_21.py
@@ -1,7 +1,4 @@
-import re, numpy
-from math import ceil
-from functools import reduce
-from itertools import combinations
+import re, numpy, ceil, itertools
 
 finder = re.compile(r'Hit Points: (\d+)[\S\s]+Damage: (\d+)[\S\s]+Armor: (\d+)')
 
@@ -10,10 +7,10 @@ with open('data/day21.txt') as fp:
 
 W = [(8,4,0),(10,5,0),(25,6,0),(40,7,0),(74,8,0)]
 A = [(0,0,0),(13,0,1),(31,0,2),(53,0,3),(75,0,4),(102,0,5)]
-R = list(combinations([(0,0,0), (25,1,0),(50,2,0),(100,3,0),(20,0,1),(40,0,2),(80,0,3)], 2))
+R = list(itertools.combinations([(0,0,0), (25,1,0),(50,2,0),(100,3,0),(20,0,1),(40,0,2),(80,0,3)], 2))
 
 C = [sum(com) for com in numpy.array([(w,a,r[0],r[1]) for w in W for a in A for r in R])]
-CALC = [(c[0], ceil(b_hp/max(1, c[1] - b_arm)), ceil(100/max(1, b_dmg - c[2]))) for c in C]
+CALC = [(c[0], math.ceil(b_hp/max(1, c[1] - b_arm)), math.ceil(100/max(1, b_dmg - c[2]))) for c in C]
 
 print("Ans 1:", [x for x in sorted(CALC, key=lambda x: x[0]) if x[1] <= x[2]][0][0])
 print("Ans 2:", [x for x in sorted(CALC, key=lambda x: x[0], reverse=True) if x[1] > x[2]][0][0])
