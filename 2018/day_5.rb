2102c9ca6e92d8e6003b3dcaf823466bc37a8b0e
Mike <mike@ring0.cc>
2018-12-06 00:17:16 +1300

    Day5 2018 - Inefficient

--- 2018/day_5.rb
+++ 2018/day_5.rb
@@ -1,6 +1,6 @@
 F = File.read("#{__dir__}/data/day5.txt").strip
 
-ξ = F.each_char.uniq.sort.each_slice(26).reduce(&:zip).map(&:join).append('Ω')[26..27].map{ |ϵ|
+ξ = F.each_char.uniq.sort.each_slice(26).reduce(&:zip).map(&:join).append('Ω').map{ |ϵ|
      (0..Float::INFINITY).lazy.each_with_object({}).take_while{ |ψ, Ξ|
           Λ = (Ξ[:α] || F.gsub(/[#{ϵ}]/, '') + '.').each_char.each_cons(2).map{|m|
                m unless m.first != m.last && (m.first === m.last.upcase || m.first.upcase === m.last)
