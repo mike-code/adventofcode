0eb4d8cc4fa5566061a328484e61abc6b73dd69b
Mike <mike@ring0.cc>
2018-12-13 11:33:51 +1300

    .

--- 2018/day_01.rb
+++ 2018/day_01.rb
@@ -1,11 +1,11 @@
-require 'set'
 F = File.read("#{__dir__}/data/day1.txt").lines.map(&:strip).map(&:to_i)
 
 puts "Ans 1: %s" % F.reduce(:+)
 
-(0..Float::INFINITY)
-    .inject(Set.new){ |gen, i|
-        v = (gen.to_a.last || 0) + F[i % F.length]
-        (puts "Ans 2: %s" % v; break) if not gen.add? v
-        gen << v
-    }
+puts "Ans 2: %s" % (0..Float::INFINITY).lazy.each_with_object([]).map{ |i, gen|
+	l = gen.last
+    v = (gen.to_a.last || 0) + F[i % F.length]
+    gen << v
+
+    v if l.nil? || (not gen.take(gen.size-2).include? l)
+}.take_while{ |m| not m.nil? }.force.last
