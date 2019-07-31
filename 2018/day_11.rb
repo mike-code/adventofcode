556bfe8054bb2f12e6d8ec22f4656ac34313f1ee
Mike <mike@ring0.cc>
2019-07-31 15:43:00 +1200

    up

--- 2018/day_11.rb
+++ 2018/day_11.rb
@@ -1,3 +1,21 @@
 OFFSET = File.read("#{__dir__}/data/day11.txt").to_i
 
+SIZE = 300
+N = 3
 
+V = Array.new(SIZE) { Array.new(SIZE, nil) }
+
+(0..(SIZE-1)).each { |x| (0..(SIZE-1)).each { |y|
+    V[x][y] = (((x+11)*(y+1)+OFFSET)*(x+11)/100).to_s.reverse[0].to_i - 5
+}}
+
+p V.map { |row| row.each_cons(N).to_a }
+   .transpose
+   .map { |q| q.each_cons(N).to_a }
+   .each_with_index.map { |row, y|
+    row.each_with_index.map { |vals, x|
+        [vals.flatten.sum, [x+1, y+1]]
+    }
+   }
+   .flatten(1)
+   .max
