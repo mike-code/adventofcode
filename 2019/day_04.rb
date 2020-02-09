8bd5aee75bb4ef0f00e4b505670a0a22b20f1d88
RETARD <mike@ring0.cc>
2020-02-09 22:32:10 +0100

    c

--- 2019/day_04.rb
+++ 2019/day_04.rb
@@ -3,5 +3,5 @@
 
 WR = Δ.map { |δ| [ [nil, *(Δ-[δ])], [[δ,δ]], [nil, *(Δ-[δ])]].inject(&:product).map(&:flatten) }.flatten(1)
 
-puts "Ans 1: %d" % Θ.select{ (@1.to_s.each_char.each_cons(2).map(&:join) & ([Δ]*2).inject(:zip).map(&:join)).any? }.size
+puts "Ans 1: %d" % Θ.select { (@1.to_s.each_char.each_cons(2).map(&:join) & ([Δ]*2).inject(:zip).map(&:join)).any? }.size
 puts "Ans 2: %d" % Θ.select { ([nil, *(@1.to_s.each_char.map(&:to_i)), nil].each_cons(4).to_a & WR).any? }.size
