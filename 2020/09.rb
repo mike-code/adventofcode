5b0e106a52b63dbf48eb814152f9866be042aae8
Mike <mike-code@users.noreply.github.com>
2020-12-09 19:14:51 +0100

    Update 09.rb

--- 2020/09.rb
+++ 2020/09.rb
@@ -1,5 +1,5 @@
 F = File.read("#{__dir__}/data/09.txt").split.map(&:to_i)
 Ξ = 25
 
-puts "Ans 1: %d" % err = F.each_cons(Ξ+1).reject { |a| a[0..Ξ-1].combination(2).map { |i| i.sum }.include?(a[Ξ]) }.first.last
-puts "Ans 2: %d" % (2..F.find_index(err)).map { |i| [*F.each_cons(i)] }.flatten(1).select { |a| a.sum == err }.first.minmax.sum
+puts "Ans 1: %d" % err = F.each_cons(Ξ+1).find { |*ϵ,ϕ| ϵ.combination(2).map(&:sum).include?(ϕ).! }.last
+puts "Ans 2: %d" % (2..F.find_index(err)).map { |i| [*F.each_cons(i)] }.flatten(1).find { |ϵ| ϵ.sum == err }.minmax.sum
